4ad0005682c4311b2a55edef19485a051251a7f2
Joseph Kofler <koflerj93@gmail.com>
2020-07-06 15:04:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,3 @@
 # don't forget to add: require 'pry'
 
-def generate_star_date
-  (rand(100000) + 400000) / 10.0
-end
 
-
-def state_log(generate_star_date)
-  puts "Captain's Log, star date #{generate_star_date}."
-end
-
-def engage
-  puts state_log(date)
-  date = generate_star_date
-end
